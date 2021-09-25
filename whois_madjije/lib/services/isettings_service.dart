9a751454c5624769ae4d77ee8f511dfacc35eb22
Boris Zivkovic <boriszivk@gmail.com>
2021-09-25 16:02:37 +0200

    settings service contract

--- whois_madjije/lib/services/isettings_service.dart
+++ whois_madjije/lib/services/isettings_service.dart
@@ -1,3 +1,28 @@
+enum NotificationType {
+  email,
+  push,
+}
+
+class NotificationSettings {
+  final NotificationType type;
+  final String? email;
+
+  NotificationSettings({
+    required this.type,
+    required this.email});
+}
+
 abstract class ISettingsService {
+
   Future<String?> getEmailForNotifications();
+  Future<void> setEmailForNotifications(String email);
+
+  Future<String> getLanguage();
+  Future<void> setLanguage(String language);
+
+  Future<NotificationSettings> getNotificationSettings();
+  Future<void> setNotificationSettings(NotificationSettings settings);
+
+  Future<bool> getDarkMode();
+  Future<void> setDarkMode(bool language);
 }
