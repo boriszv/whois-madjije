ab1089d85f98c117bdb8bdfd601273706468ebdb
Boris Zivkovic <boriszivk@gmail.com>
2021-09-25 16:17:22 +0200

    favorites service contract

---
+++ whois_madjije/lib/services/ifavorites_service.dart
@@ -0,0 +1,30 @@
+class Favorite {
+  String domainName;
+  DateTime dateTime;
+
+  Favorite({
+    required this.domainName,
+    required this.dateTime});
+
+  bool isEmpty() => domainName == '!empty!';
+
+  factory Favorite.empty() {
+    return Favorite(
+      dateTime: DateTime.now(),
+      domainName: '!empty!',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'domainName': domainName,
+      'dateTime': dateTime.toIso8601String(),
+    };
+  }
+}
+
+abstract class IFavoritesService {
+  Future<List<Favorite>> getFavorites();
+  Future<void> addFavorite(Favorite favorite);
+  Future<void> removeFromFavorites(String domain);
+}
