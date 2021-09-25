660c65a122c33a7feaab35a317939ca40355e629
Luka Stajkovic <lukastajcic@gmail.com>
2021-09-25 17:49:53 +0200

    - Home-view finishing up

--- whois_madjije/lib/views/home_view.dart
+++ whois_madjije/lib/views/home_view.dart
@@ -1,92 +1,125 @@
 import 'package:flutter/material.dart';
 
+import '../app_localizations.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final translations = AppLocalizations.of(context);
+
     return Scaffold(
-      backgroundColor: Colors.blue,
       body: Container(
-        margin: EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Text(
-              'PRIORITY LOOKUP',
-              style: TextStyle(
-                fontSize: 34,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 50),
-            Material(
-              elevation: 5.0,
-              // borderRadius: const BorderRadius.only(
-              //   topRight: Radius.circular(25),
-              //   topLeft: Radius.circular(25),
-              //   bottomRight: Radius.circular(25),
-              //   bottomLeft: Radius.circular(25),
-              // ),
-              borderRadius: const BorderRadius.all(Radius.circular(25)),
-              child: TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.white,
-                  hintText: 'Type in a domain name',
-                  hintStyle: TextStyle(
-                    color: Colors.grey.shade400,
-                  ),
-                  suffixIcon: Material(
-                    color: Theme.of(context).primaryColor,
-                    child: const Icon(Icons.chevron_right, color: Colors.white),
-                    shadowColor: Theme.of(context).primaryColor,
-                    borderRadius: const BorderRadius.only(
-                        // topRight: Radius.circular(25),
-                        // bottomRight: Radius.circular(25),
-                        ),
-                  ),
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage("assets/img/background.png"),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'PRIORITY LOOKUP',
+                style: TextStyle(
+                  fontSize: 34,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            const SizedBox(height: 50),
-            SizedBox(
-              height: 50,
-              width: 130,
-              child: TextButton(
-                onPressed: () => {},
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.search),
-                    SizedBox(width: 5),
-                    Text('Search')
+              const SizedBox(height: 50),
+
+              // TEXTFIELD
+              Material(
+                elevation: 5.0,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: const EdgeInsets.only(
+                            left: 25, bottom: 25, top: 25),
+                        hintText: translations.translate('Unesite ime domena'),
+                        hintStyle: TextStyle(
+                          color: Colors.grey.shade400,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: ElevatedButton(
+                        onPressed: () => {},
+                        child: const Icon(
+                          Icons.chevron_right,
+                          color: Colors.white,
+                        ),
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                            const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(8),
+                                bottomRight: Radius.circular(8),
+                              ),
+                            ),
+                          ),
+                          padding: MaterialStateProperty.all(
+                            const EdgeInsets.only(bottom: 25, top: 20),
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
-                style: ButtonStyle(
-                    shape: MaterialStateProperty.all(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    foregroundColor: MaterialStateProperty.all(
-                      Colors.white,
-                    ),
-                    backgroundColor: MaterialStateProperty.all(
-                      Theme.of(context).primaryColor,
-                    ),
-                    textStyle: MaterialStateProperty.all(
-                      const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),
-                    )),
               ),
-            ),
-          ],
+              const SizedBox(height: 50),
+              SizedBox(
+                height: 50,
+                width: 130,
+                child: ElevatedButton(
+                  onPressed: () => {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.search),
+                      const SizedBox(width: 5),
+                      Text(translations.translate('Pretrazi'))
+                    ],
+                  ),
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      foregroundColor: MaterialStateProperty.all(
+                        Colors.white,
+                      ),
+                      backgroundColor: MaterialStateProperty.all(
+                        Theme.of(context).primaryColor,
+                      ),
+                      textStyle: MaterialStateProperty.all(
+                        const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      )),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
